457c52a5d48c6c33acf343c90c294076680b3ec2
joseb9 <joseb9@cs.washington.edu>
2023-07-14 17:32:10 -0700

    creates .js files and updated html text

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Author: Jose Bouchereau
+ * Date: 04/14/2023
+ * The purpose of this JavaScript file is to allow for any Front-End interaction to occur.
+ * The general outline is to ensure that everything works and provide basic features for the user.
+ */
+
