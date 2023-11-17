75e2fff01c473c58d0e18fb1a777e46e0fa8323a
joseb9 <joseb9@uw.edu>
2023-11-16 21:54:05 -0800

    updated information

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-/**
- * Author: Jose Bouchereau
- * Date: 04/14/2023
- * The purpose of this JavaScript file is to allow for any Front-End interaction to occur.
- * The general outline is to ensure that everything works and provide basic features for the user.
- */
-
+//haha this does nothing too.
